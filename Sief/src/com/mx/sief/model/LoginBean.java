a30b1ff60d8b8ab32902669c9d66e9dd2bfddf97
Pako5401 <daner_sk8@hotmail.com>
2015-09-20 19:39:31 -0500

    Se crea getter y setter en LoginBean

--- Sief/src/com/mx/sief/model/LoginBean.java
+++ Sief/src/com/mx/sief/model/LoginBean.java
@@ -16,5 +16,23 @@ package com.mx.sief.model;
  * de negocio ni conexion a bd.
  */
 public class LoginBean {
+    private String user;
+    private String pass;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
     
 }
